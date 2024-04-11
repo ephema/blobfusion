5db6beeab3e663671d795d27aefc2e236bfc794e
Marc N <git@marcnitzsche.de>
2024-04-11 17:18:10 +0200

    feat: add updatedAt and createdAt fields

---
+++ prisma/migrations/20240411140428_add_created_at_and_updated_at/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `FusedBlob` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `PartialBlob` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "FusedBlob" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "PartialBlob" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
