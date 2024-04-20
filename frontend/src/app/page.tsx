dc88af2434e53feb9430e35ddf785d7c89a15465
Marc N <git@marcnitzsche.de>
2024-04-20 15:04:10 +0200

    feat(frontend): add UserInfo card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,22 @@
-import { Button } from "@/components/ui/button";
+import UserInfo from "@/components/UserInfo";
 
 export default function Home() {
   return (
     <div className="mt-20 flex h-full flex-col items-center gap-4">
-      <div className="text-center">
+      <div className="mb-4 text-center">
         <h1 className="mb-2 bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 bg-clip-text text-4xl font-bold tracking-tight text-transparent">
-          🌀 BlobFusion
+          <span className="text-white">🌀</span> BlobFusion
         </h1>
         <p className="mb-1 text-muted-foreground">
-          Making blob space affordable for everyone ✨
+          Making blob space{" "}
+          <b className="bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 bg-clip-text">
+            affordable
+          </b>{" "}
+          for everyone ✨
         </p>
       </div>
+
+      <UserInfo />
     </div>
   );
 }
---
+++ frontend/src/components/UserInfo.tsx
@@ -0,0 +1,31 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "./ui/button";
+
+const UserInfo: React.FC = () => {
+  return (
+    <Card className="min-w-[500px] bg-background/40 backdrop-blur-md">
+      <CardContent className="mt-4 flex justify-between gap-2">
+        <div className="flex flex-col gap-2">
+          <div>
+            <p className="text-muted-foreground">Account</p>
+            <h1 className="text-1xl font-bold text-foreground">
+              0x14281...14281
+            </h1>
+          </div>
+          <div>
+            <p className="text-muted-foreground">Balance</p>
+            <h1 className="text-2xl font-bold text-foreground">0.00 ETH</h1>
+          </div>
+        </div>
+        <div className="flex w-36 flex-col justify-end gap-2">
+          <Button variant="outline" className="bg-background/40">
+            Add Funds
+          </Button>
+          <Button>New Blob</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default UserInfo;
