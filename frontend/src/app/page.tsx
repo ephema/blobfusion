80cb4169c40c4b55247d9f2f83d38040e43c9a8c
Marc N <git@marcnitzsche.de>
2024-04-21 23:11:17 +0200

    fix(frontend): temporarily fix build

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -63,6 +63,8 @@ const Home = () => {
     const { amount } = values;
 
     if (chainId !== chains[0].id) {
+      // TODO fix TS
+      //@ts-expect-error
       await switchChainAsync({ chainId: chains[0].id });
     }
 
