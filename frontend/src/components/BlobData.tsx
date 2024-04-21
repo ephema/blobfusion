f774e2fedd175c37f67b1ce70f52cf03301a8f14
Marc N <git@marcnitzsche.de>
2024-04-21 09:51:28 +0200

    feat(frontend): add connect wallet button

--- frontend/src/components/BlobData.tsx
+++ frontend/src/components/BlobData.tsx
@@ -1,14 +1,21 @@
 import FusedBlobList from "@/components/FusedBlobList";
 import UnfusedBlobList from "@/components/UnfusedBlobList";
 import { PartialBlob } from "@/lib/types";
+import { cn } from "@/lib/utils";
 
 interface BlobDataProps {
   blobs: PartialBlob[];
 }
 
 const BlobData: React.FC<BlobDataProps> = ({ blobs }) => {
+  const isLoggedIn = false;
+
   return (
-    <div className="flex max-w-full flex-col gap-8 lg:mt-8 lg:flex-row lg:gap-16">
+    <div
+      className={cn(
+        "flex max-w-full flex-col gap-8 lg:mt-8 lg:flex-row lg:gap-16",
+      )}
+    >
       <UnfusedBlobList blobs={blobs} />
       <FusedBlobList blobs={blobs} />
     </div>
--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -1,20 +1,31 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 const UserInfo = () => {
+  const isLoggedIn = false;
+  const placeholderAddress = "0x12345...54321";
+  const address = isLoggedIn ? "0x14281...14281" : placeholderAddress;
+  const balance = "0.00";
+
   return (
     <Card className="w-[500px] max-w-full bg-background/40 backdrop-blur-md">
-      <CardContent className="mt-4 flex justify-between gap-2">
+      <CardContent
+        className={cn(
+          "mt-4 flex justify-between gap-2",
+          !isLoggedIn && "blur-[2px]",
+        )}
+      >
         <div className="flex flex-col gap-2">
           <div>
             <p className="text-muted-foreground">Account</p>
-            <h1 className="text-1xl font-bold text-foreground">
-              0x14281...14281
-            </h1>
+            <h1 className="text-1xl font-bold text-foreground">{address}</h1>
           </div>
           <div>
             <p className="text-muted-foreground">Balance</p>
-            <h1 className="text-2xl font-bold text-foreground">0.00 ETH</h1>
+            <h1 className="text-2xl font-bold text-foreground">
+              {balance} ETH
+            </h1>
           </div>
         </div>
         <div className="flex w-36 flex-col justify-center gap-2">
@@ -24,6 +35,12 @@ const UserInfo = () => {
           <Button>New Blob</Button>
         </div>
       </CardContent>
+
+      {!isLoggedIn && (
+        <div className="absolute inset-0 flex items-center justify-center rounded-md bg-background/40">
+          <Button>Connect Wallet</Button>
+        </div>
+      )}
     </Card>
   );
 };
